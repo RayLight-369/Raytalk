a8a62c6993736d8c6d83f71478215718e3c809be
Ray <buttrafay980@gmail.com>
2024-08-25 19:32:39 +0500

    update

--- src/socketio.js
+++ src/socketio.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io( "http://localhost:5261", {
+export const socket = io( "https://next-chat-socket.onrender.com", {
   autoConnect: false
 } );
 
