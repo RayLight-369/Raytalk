e720982e06c0dcf3101c35b0d2bede8513322167
Ray <buttrafay980@gmail.com>
2024-08-27 13:40:33 +0500

    update

--- src/Contexts/Messages.js
+++ src/Contexts/Messages.js
@@ -19,10 +19,41 @@ const Messages = ( { children } ) => {
 
   useEffect( () => {
 
+    function requestNotificationPermission () {
+      if ( 'Notification' in window ) {
+        Notification.requestPermission().then( ( permission ) => {
+          if ( permission === 'granted' ) {
+            console.log( 'Notification permission granted.' );
+          } else {
+            console.log( 'Notification permission denied.' );
+          }
+        } );
+      } else {
+        console.log( 'Browser does not support notifications.' );
+      }
+    }
+
+    requestNotificationPermission();
+
+    function showNotification ( title, options ) {
+      if ( Notification.permission === 'granted' ) {
+        new Notification( title, options );
+      } else {
+        console.log( 'Notification permission not granted.' );
+      }
+    }
+
+
     socket.connect();
 
     socket.on( "msg", ( msg, fromID, fromName, media, date ) => {
       setMsgs( prev => ( [ ...prev, { value: msg, fromID, fromName, media, type: "msg", date } ] ) );
+
+      showNotification( `**${ fromName }**`, {
+        body: msg,
+        icon: document.querySelector( "link[rel*='icon']" )?.href || "/favicon.ico", // Optional: Path to your notification icon
+        tag: 'message-notification', // Optional: Tag to uniquely identify the notification
+      } );
     } );
 
     socket.on( "note", ( id, name, type, totalUsers ) => {
